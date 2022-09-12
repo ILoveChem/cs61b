f0b15f354343e715af83766447b3823c372a4762
Xiao Zizhen <xiaozzh@outlook.com>
2022-09-12 12:12:46 +0800

    modified cacenate and dcacenate methods for null input.

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,9 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if (A == null) {
+            return B;
+        }
         IntList pointer = A;
         while (pointer.rest != null) {
             pointer = pointer.rest;
@@ -94,6 +97,9 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
+        if (A == null || B == null) {
+            return null;
+        }
         IntList newList = new IntList();
         IntList pointer = newList;
         while (A != null) {
